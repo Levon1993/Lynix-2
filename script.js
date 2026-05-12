89ffde4e5c41961d3b4da749ac8e04650852e472
Levon1993 <styleitup.info@yahoo.com>
2026-05-12 19:10:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -173,3 +173,27 @@ document.addEventListener("DOMContentLoaded", () => {
   }, 3000);
 
 });
+
+const targetDate = new Date();
+
+// Setze Ziel: z.B. 5 Tage in Zukunft
+targetDate.setDate(targetDate.getDate() + 5);
+
+function updateCountdown() {
+
+  const now = new Date().getTime();
+  const distance = targetDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
+}
+
+setInterval(updateCountdown, 1000);
+updateCountdown();
